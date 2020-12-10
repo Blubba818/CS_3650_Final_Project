ac3e66fb74c4e1f73de7fffdc2b91e45aeb6ef3b
Bryson Campbell <brysoncampbell@mail.weber.edu>
2020-12-10 03:38:01 -0700

    ui fixes - prepped for styling

--- final/finalproj/finalproj/urls.py
+++ final/finalproj/finalproj/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('register/', user_views.register, name="register"),
     path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
+
+urlpatterns += [] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
